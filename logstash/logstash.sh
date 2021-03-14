5ef346704994f61a9763efd8ad48d70a68e8f922
piggona <2803302887@qq.com>
2021-03-14 17:15:30 +0800

    fix

--- logstash/logstash.sh
+++ logstash/logstash.sh
@@ -1,7 +1,7 @@
 docker run --rm --name=logstash \
 --privileged=true \
--v /home/haohao/fundingsService/logstash/pipeline:/usr/share/logstash/pipeline/ \
--v /home/haohao/fundingsService/logstash/logstash.yml:/usr/share/logstash/config/logstash.yml \
---net cluster_elastic \
+-v /home/haohao/go/src/github.com/piggona/fundingsService/logstash/pipeline:/usr/share/logstash/pipeline/ \
+-v /home/haohao/go/src/github.com/piggona/fundingsService/logstash/logstash.yml:/usr/share/logstash/config/logstash.yml \
+--net mini_cluster_elastic \
 -d \
-docker.elastic.co/logstash/logstash:7.5.0
+logstash:7.5.0
